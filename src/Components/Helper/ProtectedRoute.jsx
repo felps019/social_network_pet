8da4933729e61ffd87a28e3ea6310712906b7fc7
felps019 <felipedocs313@gmail.com>
2024-10-24 09:36:25 -0300

    feat: protected route in account

---
+++ src/Components/Helper/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { UserContext } from '../../UserContext'
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({children}) => {
+  const { login } = React.useContext(UserContext);
+  // Esse children pega o componente filho do protectedRoute
+  // Nesse caso ele pega o User que esta dentro do protectedRoute em App.jsx
+  return login ? children : <Navigate to="/login" /> ;
+}
+
+export default ProtectedRoute
